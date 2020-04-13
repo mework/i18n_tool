8ecc9c0e10205f80fabe8133f6fe889c201a566a
jiqinxiong <canmeng_web@163.com>
2020-04-13 16:27:28 +0800

    添加输出国际化差异，并翻译的模式

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import {
   ms,
 } from './lang';
 import { util, errorLog, changeLog } from './utils';
+import { transformKey, mode } from './config';
 import fs from 'fs';
 
 const del = require('del');
@@ -16,17 +17,6 @@ const {
   translate
 } = util;
 
-// 语种  ru：俄语、ko：韩语、en：英语、zh-TW：中文（繁体）、ja：日语、zh：中文
-const tlKey = {
-  ru: 'ru',
-  ko: 'ko',
-  en: 'en',
-  tw: 'zh-TW',
-  ja: 'ja',
-  zh: 'zh',
-  ms: 'ms',
-};
-
 const fromLang = zh,
   from = JSON.parse(JSON.stringify(fromLang));
 
@@ -34,7 +24,8 @@ let toLang,  // 要转换到的语言
   to,
   toLangKey, // 转换的语言key值和名字
   toLangGoogleKey, // google翻译语言的key值
-  fromTextArr; // 源文本数组
+  fromTextArr, // 源文本数组
+  changeObj = {}; // 改变的对象
 
 /**
  * 循环处理数据
@@ -55,23 +46,54 @@ function loop(key, value, position) {
 
   let formCurrent = JSON.parse(JSON.stringify(from)); // 转换前的原有数据
   let toCurrent = to; // 转换后的原有数据
+  let diffCurrent = changeObj;  // 找出差异的数据对象
 
   position.forEach(async (item, index) => {
     formCurrent = formCurrent[item];
     let _toCurrent = toCurrent[item];
+    // 区分模式
+    if (mode === 'default') {
+      // 判断进行转换的是否有此值，如果不是数组最后一项，就强行干个对象上去
+      if (!_toCurrent && (position.length - 1 !== index)) return toCurrent = toCurrent[item] = {};
 
-    // 判断进行转换的是否有此值，如果不是数组最后一项，就强行干个对象上去
-    if (!_toCurrent && (position.length - 1 !== index)) return toCurrent = toCurrent[item] = {};
+      // 取到数组最后一项才得到值
+      if (!_toCurrent && position.length - 1 === index) {
+        fromTextArr.push(formCurrent);
+        
+        // 赋值操作
+        toCurrent[item] = `{{%${ formCurrent }%}}`;
+        
+        // 记录改动点
+        changeLog.info(`${ toLangKey } 改动点：${ position.join('>') }` );
+      }
 
-    // 取到数组最后一项才得到值
-    if (!_toCurrent && position.length - 1 === index) {
-      !toCurrent[item] && fromTextArr.push(formCurrent);
-      
-      // 赋值操作
-      toCurrent[item] = `{{%${ formCurrent }%}}`;
-      
-      // 记录改动点
-      changeLog.info(`${ toLangKey } 改动点：${ position.join('>') }` );
+    } else if (mode === 'findDiff') {
+      // 判断进行转换的是否有此值，如果不是数组最后一项，就强行干个对象上去
+      if (!_toCurrent && (position.length - 1 !== index)) return toCurrent = toCurrent[item] = {};
+
+      // 转换后的没有值，需要赋值对象
+      if (!_toCurrent) {
+        position.forEach((subitem, subindex) => {
+          if (diffCurrent[subitem] !== undefined && position.length - 1 !== subindex) return diffCurrent = diffCurrent[subitem];
+          
+          if (position.length - 1 !== subindex) {
+            diffCurrent = diffCurrent[subitem] = {};
+          }
+        })
+      }
+
+
+      // 取到数组最后一项才得到值
+      if (!_toCurrent && position.length - 1 === index) {
+        fromTextArr.push(formCurrent);
+        
+        // 赋值操作
+        diffCurrent[item] = `{{%${ formCurrent }%}}`;
+        
+        // 记录改动点
+        changeLog.info(`${ toLangKey } 改动点：${ position.join('>') }` );
+      }
+
     }
 
     // 移动 toCurrent 指针
@@ -88,6 +110,11 @@ function start() {
     for (const [key, item] of Object.entries(from)) {
       loop(key, item, [key]);
     }
+
+    if (fromTextArr.length <= 0) {
+      return console.log('文件无需翻译！！');
+    }
+
     // 将数据处理为请求文本格式
     let requestText = fromTextArr.join('\r\n');
     // 翻译后的文本数组
@@ -96,19 +123,40 @@ function start() {
     await translate(requestText, toLangGoogleKey).then(async res => {
       if (!res) return;
 
-      // 获取翻译后的文本数组
-      toTextArr = res.map(item => (item[0].replace(/\r\n/g, '')));
-      to = JSON.stringify(to);
-
-      // 替换文本并转化中文双引号为单引号
-      fromTextArr.forEach((item, index) => {
-        to = to.replace(new RegExp(`{{%${item}%}}`, 'g'), toTextArr[index].replace(/"/g, ''));
-      })
-
-      // 写入文本
-      to = `export default ${to}`;
       
-      fs.writeFileSync(`./build/${ toLangKey }.js`, to);
+
+      if (mode === 'default') {
+
+        // 获取翻译后的文本数组
+        toTextArr = res.map(item => (item[0].replace(/\r\n/g, '')));
+
+        to = JSON.stringify(to);
+
+        // 替换文本并转化中文双引号为单引号
+        fromTextArr.forEach((item, index) => {
+          to = to.replace(new RegExp(`{{%${item}%}}`, 'g'), toTextArr[index].replace(/"/g, ''));
+        })
+
+        // 写入文本
+        to = `export default ${to}`;
+        
+        fs.writeFileSync(`./build/${ toLangKey }.js`, to);
+
+      } else if (mode === 'findDiff') {
+        
+        changeObj = JSON.stringify(changeObj);
+
+        // 替换文本并转化中文双引号为单引号
+        fromTextArr.forEach((item, index) => {
+          changeObj = changeObj.replace(new RegExp(`{{%${item}%}}`, 'g'), toTextArr[index].replace(/"/g, ''));
+        })
+
+        // 写入文本
+        changeObj = `export default ${changeObj}`;
+        
+        fs.writeFileSync(`./build/${ toLangKey }.js`, changeObj);
+
+      }
 
       console.log(`${ toLangKey }写入完成`);
       resolve();
@@ -144,8 +192,9 @@ async function run() {
     toLang = item.source;
     to = JSON.parse(JSON.stringify(toLang));
     toLangKey = item.key;
-    toLangGoogleKey = tlKey[toLangKey];
+    toLangGoogleKey = transformKey[toLangKey];
     fromTextArr = [];
+    changeObj = {};
     changeLog.info(`${ toLangKey } 改动点开始 -----------------------------------------------------` );
     await start();
     changeLog.info(`${ toLangKey } 改动点结束 -----------------------------------------------------` );
---
+++ config.js
@@ -0,0 +1,13 @@
+// 语种key值转换  ru：俄语、ko：韩语、en：英语、zh-TW：中文（繁体）、ja：日语、zh：中文
+export const transformKey = {
+  ru: 'ru',
+  ko: 'ko',
+  en: 'en',
+  tw: 'zh-TW',
+  ja: 'ja',
+  zh: 'zh',
+  ms: 'ms',
+}
+
+// 模式  default:全翻译、findDiff:找出差异
+export const mode = 'default';
